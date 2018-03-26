3cde3091ee159b5fb0092dfd0b214ba6a28e94ea
Emre <emrsmsrli1@gmail.com>
2018-03-26 23:07:50 +0300

    add onFrame() getResources() and className() for debug

--- pluginapi/src/main/java/tr/edu/iyte/vrxd/api/IPlugin.kt
+++ pluginapi/src/main/java/tr/edu/iyte/vrxd/api/IPlugin.kt
@@ -3,12 +3,17 @@ package tr.edu.iyte.vrxd.api
 import android.content.Context
 
 interface IPlugin {
-    fun className() = this.javaClass.name
+    fun className() = this.javaClass.name!!
 
     fun onStart(ctx: Context)
     fun onResume(ctx: Context)
     fun onPause(ctx: Context)
     fun onStop(ctx: Context)
 
+    /*fun onConfigurationRequested()
+    fun onConfigure(configs: Con)*/
 
+    fun onFrame(bytes: ByteArray)
+
+    fun getResources(): List<String>
 }
